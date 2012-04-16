cfc815028001d525bc27059dcd7e1b775b4ccead
Dan Hill <dan@crashpadder.com>
2012-04-16 21:59:56 +0100

    Directory fix

--- bin/resque_pool_ctl.rb
+++ bin/resque_pool_ctl.rb
@@ -3,7 +3,7 @@ require 'daemons'
 
 dir = File.dirname(__FILE__)
 if ENV['RAILS_ENV'] == "production"
-	Daemons.run(dir + '/resque_pool.rb', {:log_output => true, :multiple => false, :log_dir => "/home/ubuntu/shared/log", :dir_mode => :normal, :dir => '/home/ubuntu/shared/pids'})
+	Daemons.run(dir + '/resque_pool.rb', {:log_output => true, :multiple => false, :log_dir => "/home/ubuntu/crisisly/shared/log", :dir_mode => :normal, :dir => '/home/ubuntu/crisisly/shared/pids'})
 else
 	Daemons.run(dir + '/resque_pool.rb', {:log_output => true, :multiple => false, :log_dir => ".", :dir_mode => :script, :dir => '.'})
 end
--- bin/twitterfeed_ctl.rb
+++ bin/twitterfeed_ctl.rb
@@ -3,4 +3,4 @@ require 'daemons'
 
 dir = File.dirname(__FILE__)
 
-Daemons.run(dir + '/twitterfeed.rb', {:log_output => true, :multiple => false, :log_dir => "/home/ubuntu/shared/log", :dir_mode => :normal, :dir => '/home/ubuntu/shared/pids'})
+Daemons.run(dir + '/twitterfeed.rb', {:log_output => true, :multiple => false, :log_dir => "/home/ubuntu/crisisly/shared/log", :dir_mode => :normal, :dir => '/home/ubuntu/crisisly/shared/pids'})
